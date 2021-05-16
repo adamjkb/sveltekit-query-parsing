08c84f980092b1d552e709a3d66edbe1bece49bc
adamjkb <dmynet@gmail.com>
2021-05-16 17:52:58 +0100

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script context='module'>
+export async function load({ page }) {
+
+    return {
+        props: {
+            comma: page.query.get('comma'),
+            simple: page.query.get('simple'),
+        },
+        status: 200,
+    }
+}
+</script>
+<script>
+  export let comma, simple
+</script>
+
+Comma query parameter:
+{comma}
+<br/>
+Simple query parameter:
+{simple}
